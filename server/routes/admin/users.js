f1cc8f350f993b2b3c2d3af6d69e11e8bb7ccc4d
Malcolm Chinedu Nnachi <115088011+sweetboy-crypto@users.noreply.github.com>
2025-08-24 19:46:22 -1100

    Update users.js

--- server/routes/admin/users.js
+++ server/routes/admin/users.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../../controllers/admin/usersController');
+const { getAllUsers, deleteUser, updateUser } = require('../../controllers/admin/usersController');
 const { protect, admin } = require('../../middleware/authMiddleware');
 
 // @route   GET /api/admin/users
 // @desc    Get all users
 // @access  Private/Admin
 router.get('/', protect, admin, getAllUsers);
+
+// @route   DELETE /api/admin/users/:id
+// @desc    Delete a user
+// @access  Private/Admin
+router.delete('/:id', protect, admin, deleteUser);
+
+// @route   PUT /api/admin/users/:id
+// @desc    Update a user
+// @access  Private/Admin
+router.put('/:id', protect, admin, updateUser);
 
 module.exports = router;
